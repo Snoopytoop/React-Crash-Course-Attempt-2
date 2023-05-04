027d9fbdd7f26a930fcfff52d7ef9ad7f08fe0ae
Snoopytoop <samuelluke1998@gmail.com>
2023-05-04 17:36:33 +0100

    start

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>My Todo List</h1>
+      <div>
+        <h2> Finish Frontend Simplified</h2>
+      </div>
+    </div>
+  );
+}
+
+export default App;
